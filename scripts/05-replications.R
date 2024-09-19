4c099b4c3d2c74096dfd8d0feeb6e16a7f17e269
RIRI0527 <159666740+RIRI0527@users.noreply.github.com>
2024-09-19 18:03:59 -0400

    Add files via upload

---
+++ scripts/05-replications.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Sanity check of the data
+# Author: Ruizi Liu
+# Date: 19 September 2024
+# Contact: ruizi.liu@utoronto.ca
+# License: MIT
+# Pre-requisites: Need to have simulated data
+# Any other information needed? None.
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
